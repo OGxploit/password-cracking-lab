fdc1840e1208e65767263a13dbcdbc8513058f20
OGxploit <i.k.krijezi@gmail.com>
2025-06-15 13:36:32 +0200

    Update main.py to support Hydra SSH brute-force

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import argparse
 from src.john_runner import crack_hash_with_john
+from src.hydra_runner import run_hydra_ssh
 
 def main():
     parser = argparse.ArgumentParser(description="Password Cracking Lab")
     parser.add_argument('--john', help='Crack hashes using John the Ripper')
     parser.add_argument('--wordlist', help='Path to the wordlist file')
 
+    parser.add_argument('--hydra-host', help='Target host for Hydra (e.g. IP or domain)')
+    parser.add_argument('--hydra-user', help='Username to brute-force (e.g. root)')
+
     args = parser.parse_args()
 
     if args.john and args.wordlist:
-        result = crack_hash_with_john(args.john, args.wordlist)
-        print(result)
-    else:
-        print("[!] Usage: --john <hashfile> --wordlist <wordlist>")
+        print(crack_hash_with_john(args.john, args.wordlist))
+
+    if args.hydra_host and args.hydra_user and args.wordlist:
+        print(run_hydra_ssh(args.hydra_host, args.hydra_user, args.wordlist))
 
 if __name__ == '__main__':
     main()
